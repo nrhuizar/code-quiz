c96884627960864a7719da062d4a541362d43108
nrhuizar <nrhuizar@gmail.com>
2021-03-24 20:07:25 -0700

    updated questions

--- script.js
+++ script.js
@@ -1,38 +1,23 @@
-// // timer
-// const timeLeft = 100;
-// const timer = document.getElementById('timer');
-// const timerId = setInterval(countdown, 1000);
-
-// function countdown() {
-//     if (timeLeft == -1) {
-//         clearTimeout(timerId);
-
-//     } else {
-//         timer.innerHTML = timeLeft + ' seconds remaining';
-//         timeLeft--;
-//     }
-// }
-
-let questions = [
-    { question: "What does the D in CRUD stand for?", answer: "Delete Destroy Dance Deploy", answerCheck: 0 },
-    { question: "What version was the arrow function introduced?", answer: "ES4 ES5 ES6 ES7", answerCheck: 2 },
-    { question: "What property is used to access a location in an object?", answer: "Index Rout Key Claw", answerCheck: 2 },
-    { question: "What is used to access a location in an array?", answer: "Index Rout Key Claw", answerCheck: 0 },
+const questions = [
+    { question: "Which is NOT a part of CRUD?", answer: "Create Repeat Update Deconste", answerCheck: 1 },
+    { question: "Which reference means this or this?", answer: "=== && || //", answerCheck: 2 },
+    { question: "Arrays in JavaScript contain ___.", answer: "data secrets everything conditionals", answerCheck: 0 },
+    { question: "Where push and pull front end data?", answer: "localStorage reality word.doc localSession", answerCheck: 0 },
     { question: "What is the html landing page file named?", answer: "lp.html home.html front.html index.html", answerCheck: 3 },
     { question: "What is used to style an html page?", answer: "JS CSS API CRUD", answerCheck: 1 },
   ];
-let score = 0;
-let i = 0;
-let t = 120
-let body = document.body;
-let splitAnswer = questions[i].answer.split(" ");
-let currentQuestion = questions[i].question
+const score = 0;
+const i = 0;
+const t = 100
+const body = document.body;
+const splitAnswer = questions[i].answer.split(" ");
+const currentQuestion = questions[i].question
 
-let Q = document.querySelector('#question')
-let A1 = document.querySelector('#a')
-let A2 = document.querySelector('#b')
-let A3 = document.querySelector('#c')
-let A4 = document.querySelector('#d')
+const Q = document.querySelector('#question')
+const A1 = document.querySelector('#a')
+const A2 = document.querySelector('#b')
+const A3 = document.querySelector('#c')
+const A4 = document.querySelector('#d')
 
 localStorage.setItem('youGotHighScore', false)
 
@@ -45,7 +30,7 @@ A3.textContent = "3. " + splitAnswer[2]
 A4.textContent = "4. " + splitAnswer[3]
 
 
-let changeAnswers = (answerValue) => {
+const changeAnswers = (answerValue) => {
     if (questions[i].answerCheck == answerValue) {
         score++;
         console.log(score, 'Current correct answers.');
@@ -79,8 +64,8 @@ A4.addEventListener('click', () => {
 });
 
 //timer
-let timer = document.querySelector('#timer');
-let goToNewPage = false;
+const timer = document.querySelector('#timer');
+const goToNewPage = false;
 timer.textContent = 'Timer: ' + t + ' seconds';
 
 setInterval ( () => { 
@@ -97,12 +82,12 @@ setInterval ( () => {
             var initials = prompt('What are your initials?');
         }
 
-    let check = localStorage.getItem('check');
+    const check = localStorage.getItem('check');
 
     //if check = checked then there is a list of players
     if (check === 'checked') {
-        let localStoragePlayers = localStorage.getItem('players');
-        let highScore = localStoragePlayers.split('/');
+        const localStoragePlayers = localStorage.getItem('players');
+        const highScore = localStoragePlayers.split('/');
 
         //If the players current score is > than the high score update high score
         if (finalScore > highScore[1]){
@@ -131,3 +116,22 @@ if (goToNewPage === true) {
     window.location='./highscore.html';
 }
 }, 1000);
+
+const recentScore = localStorage.getItem('recentScore');
+console.log(recentScore);
+
+const yourScore = document.querySelector('.score');
+if (recentScore){
+yourScore.textContent = 'Your Score: ' + recentScore;
+}
+const showHighScore = localStorage.getItem('players').split('/');
+    const highScoreInitials = document.querySelector('#initials');
+    const highScoreScore = document.querySelector('#highScore');
+    highScoreInitials.textContent = "Initials: " + showHighScore[0].toUpperCase();
+    highScoreScore.textContent = "Score: " + showHighScore[1];
+
+
+const youGotHighScore = document.querySelector('h1');
+if (localStorage.getItem('youGotHighScore') == 'true'){
+    youGotHighScore.textContent = 'You Got The High Score!'
+}
